f27d1970f83dc9d10ae6149a296adde14c7f5d80
Chris Kalmar <christian.kalmar@gmail.com>
2024-03-01 22:41:31 +0100

    define first routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+  .get("/", () => "Hello 👋, I'm Mock Mirror")
+
+  .all("*", ({ path, headers, set }) => {
+    console.log(path, headers);
+
+    set.status = "Not Found";
+  })
+
+  .listen(Bun.env.PORT || 3210);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🪞 Mock Mirror is running at ${app.server?.hostname}:${app.server?.port}`
 );
