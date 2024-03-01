078b69bcd673a04aa2cdc643c55446217da2d4c2
Chris Kalmar <christian.kalmar@gmail.com>
2024-03-01 23:11:52 +0100

    reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
 const app = new Elysia()
 
-  .get("/", () => "Hello 👋, I'm Mock Mirror")
+  .get('/', () => "Hello 👋, I'm Mock Mirror")
 
-  .all("*", ({ path, headers, set }) => {
+  .all('*', ({ path, headers, set }) => {
     console.log(path, headers);
 
-    set.status = "Not Found";
+    set.status = 'Not Found';
   })
 
   .listen(Bun.env.PORT || 3210);
 
-console.log(
-  `🪞 Mock Mirror is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🪞 Mock Mirror is running at ${app.server?.hostname}:${app.server?.port}`);
--- src/registry.ts
+++ src/registry.ts
@@ -1,18 +1,8 @@
-import { DEFAULT_SCOPE } from "./const";
+import { DEFAULT_SCOPE } from './const';
 
 type MockedRoute = {
   pathPattern: string;
-  method:
-    | "GET"
-    | "POST"
-    | "PUT"
-    | "DELETE"
-    | "PATCH"
-    | "OPTIONS"
-    | "HEAD"
-    | "CONNECT"
-    | "TRACE"
-    | "ALL";
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'OPTIONS' | 'HEAD' | 'CONNECT' | 'TRACE' | 'ALL';
   response: unknown;
   status: number;
   headers: Record<string, string>;
@@ -26,24 +16,12 @@ const registry = new Map<string, MockedRoutes>();
 
 registry.set(DEFAULT_SCOPE, []);
 
-export const addMockedRoute = ({
-  scope,
-  route,
-}: {
-  scope: string;
-  route: MockedRoute;
-}) => {
+export const addMockedRoute = ({ scope, route }: { scope: string; route: MockedRoute }) => {
   const routes = registry.get(scope) || [];
 
   registry.set(scope, [route, ...routes]);
 };
 
-export const addMockedRoutes = ({
-  scope,
-  routes,
-}: {
-  scope: string;
-  routes: MockedRoute[];
-}) => {
+export const addMockedRoutes = ({ scope, routes }: { scope: string; routes: MockedRoute[] }) => {
   routes.forEach((route) => addMockedRoute({ scope, route }));
 };
