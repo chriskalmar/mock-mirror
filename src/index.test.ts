221bfee9436d13aafaecd4427d6c8627056d85bc
Chris Kalmar <christian.kalmar@gmail.com>
2024-03-02 15:24:04 +0100

    test adding and retrieving mock

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,16 @@
-import { describe, expect, it } from 'bun:test';
+import { beforeEach, describe, expect, it } from 'bun:test';
 import { edenTreaty } from '@elysiajs/eden';
 import { app } from '.';
+import { MOCK_MIRROR_HEADER } from './const';
 
-const api = edenTreaty<typeof app>(`http://localhost:${app.server?.port}`);
+const serverUrl = `http://localhost:${app.server?.port}`;
+const api = edenTreaty<typeof app>(serverUrl);
 
 describe('server', () => {
+  beforeEach(async () => {
+    await api['mock-mirror'].reset.post();
+  });
+
   it('should return error if missing input for adding routes', async () => {
     // @ts-expect-error input is omitted deliberately
     const { data, error, status, response } = await api['mock-mirror'].add.post();
@@ -17,5 +23,38 @@ describe('server', () => {
     const { data } = await api['mock-mirror'].add.post({ scope: 'scope-one', routes: [] });
 
     expect(data).toMatchSnapshot();
+  });
+
+  it('should be able to respond with mock', async () => {
+    {
+      const { data } = await api['mock-mirror'].add.post({
+        scope: 'scope-one',
+        routes: [
+          {
+            pathPattern: '/api/users/*',
+            method: 'GET',
+            response: 'any user',
+            status: 200,
+          },
+        ],
+      });
+
+      expect(data).toMatchSnapshot('add mock route');
+    }
+
+    {
+      const response = await app
+        .handle(
+          new Request(`${serverUrl}/api/users/777`, {
+            method: 'GET',
+            headers: {
+              [MOCK_MIRROR_HEADER]: 'scope-one',
+            },
+          }),
+        )
+        .then((res) => res.text());
+
+      expect(response).toMatchSnapshot('mock response');
+    }
   });
 });
