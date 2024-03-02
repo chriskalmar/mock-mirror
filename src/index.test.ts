5e963082a211b648d703d0f4406ad38211b17ee1
Chris Kalmar <christian.kalmar@gmail.com>
2024-03-02 15:07:09 +0100

    move to eden treaty for running integration tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,38 +2,20 @@ import { describe, expect, it } from 'bun:test';
 import { edenTreaty } from '@elysiajs/eden';
 import { app } from '.';
 
-const mockMirrorPath = 'http://localhost/mock-mirror';
-const defaultPath = 'http://localhost/';
+const api = edenTreaty<typeof app>(`http://localhost:${app.server?.port}`);
 
 describe('server', () => {
   it('should return error if missing input for adding routes', async () => {
-    const addRoutes = await app
-      .handle(
-        new Request(`${mockMirrorPath}/add`, {
-          method: 'POST',
-        }),
-      )
-      .then((res) => res.text());
+    // @ts-expect-error input is omitted deliberately
+    const { data, error, status, response } = await api['mock-mirror'].add.post();
 
-    expect(addRoutes).toInclude('Expected object');
+    expect(status).toBe(400);
+    expect(data).toInclude('Expected object');
   });
 
   it('should add routes to scope if provided', async () => {
-    const addRoutes = await app
-      .handle(
-        new Request(`${mockMirrorPath}/add`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            scope: 'ff71ae5d-3112-4321-a9e1-459f72a453c0',
-            routes: [],
-          }),
-        }),
-      )
-      .then((res) => res.json());
+    const { data } = await api['mock-mirror'].add.post({ scope: 'scope-one', routes: [] });
 
-    expect(addRoutes).toMatchSnapshot();
+    expect(data).toMatchSnapshot();
   });
 });
