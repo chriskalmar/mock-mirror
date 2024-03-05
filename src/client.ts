503849830736745a2e597b439ba5c86a459c89bc
Chris Kalmar <christian.kalmar@gmail.com>
2024-03-05 18:21:15 +0100

    add tools to client

--- src/client.ts
+++ src/client.ts
@@ -2,28 +2,39 @@ import { edenTreaty } from '@elysiajs/eden';
 import { app } from '.';
 import { randomUUID } from 'crypto';
 import { MOCK_MIRROR_HEADER } from './const';
+import { MockedRoute, MockedRoutes } from './types';
 
 export const createMockMirror = async ({ mockMirrorUrl }: { mockMirrorUrl?: string }) => {
   const api = edenTreaty<typeof app>(mockMirrorUrl ?? Bun.env.MOCK_MIRROR_URL ?? 'http://localhost:3210');
 
-  const getTools = ({ scope = randomUUID() }: { scope: string }) => ({
+  const getTools = ({ scope }: { scope: string }) => ({
     addRoute: async (route: MockedRoute) => {
       return await api['mock-mirror'].add.post({ scope, routes: [route] });
     },
+    addRoutes: async (routes: MockedRoutes) => {
+      return await api['mock-mirror'].add.post({ scope, routes });
+    },
     clearScope: async () => {
-      return await api['mock-mirror'].reset.post({ scope });
+      return await api['mock-mirror']['clear-scope'].post({ scope });
+    },
+    reset: async () => {
+      return await api['mock-mirror'].reset.post();
     },
   });
 
   return (
-    integrationTest: (params: typeof tools) => unknown,
+    integrationTest: (tools: ReturnType<typeof getTools>) => unknown,
     options?: {
       scope?: string;
     },
   ) => {
     const scope = options?.scope ?? randomUUID();
 
-    return integrationTest(tools);
+    return integrationTest(
+      getTools({
+        scope,
+      }),
+    );
   };
 };
 
